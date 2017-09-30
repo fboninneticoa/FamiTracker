6ee766fe7fa3a92431fe832eeabc12af4d628398
Roger <narayanroger@gmail.com>
2017-09-30 17:57:52 -0400

    Update APU files to the latest version
    Available on GitHub:
    * https://github.com/digital-sound-antiques/emu2149
    * https://github.com/digital-sound-antiques/emu2413

--- Source/APU/emu2149.h
+++ Source/APU/emu2149.h
@@ -2,22 +2,7 @@
 #ifndef _EMU2149_H_
 #define _EMU2149_H_
 
-#ifndef _MAIN_H_
-typedef unsigned int uint32;
-typedef int int32;
-typedef signed short int16;
-typedef unsigned short uint16;
-typedef signed char int8;
-typedef unsigned char uint8;
-#endif
-
-#ifdef EMU2149_DLL_EXPORTS
-#define EMU2149_API __declspec(dllexport)
-#elif  EMU2149_DLL_IMPORTS
-#define EMU2149_API __declspec(dllimport)
-#else
-#define EMU2149_API
-#endif
+#include <stdint.h>
 
 #define EMU2149_VOL_DEFAULT 1
 #define EMU2149_VOL_YM2149 0
@@ -30,71 +15,72 @@ extern "C"
 {
 #endif
 
-typedef struct __PSG
-{
-	/* Volume Table */
-	uint32* voltbl;
+  typedef struct __PSG
+  {
 
-	uint8 reg[0x20];
-	int32 out;
-	int32 cout[3];
+    /* Volume Table */
+    uint32_t *voltbl;
 
-	uint32 clk, rate, base_incr, quality;
+    uint8_t reg[0x20];
+    int32_t out;
 
-	uint32 count[3];
-	uint32 volume[3];
-	uint32 freq[3];
-	uint32 edge[3];
-	uint32 tmask[3];
-	uint32 nmask[3];
-	uint32 mask;
+    uint32_t clk, rate, base_incr, quality;
 
-	uint32 base_count;
+    uint32_t count[3];
+    uint32_t volume[3];
+    uint32_t freq[3];
+    uint32_t edge[3];
+    uint32_t tmask[3];
+    uint32_t nmask[3];
+    uint32_t mask;
 
-	uint32 env_volume;
-	uint32 env_ptr;
-	uint32 env_face;
+    uint32_t base_count;
 
-	uint32 env_continue;
-	uint32 env_attack;
-	uint32 env_alternate;
-	uint32 env_hold;
-	uint32 env_pause;
-	uint32 env_reset;
+    uint32_t env_volume;
+    uint32_t env_ptr;
+    uint32_t env_face;
 
-	uint32 env_freq;
-	uint32 env_count;
+    uint32_t env_continue;
+    uint32_t env_attack;
+    uint32_t env_alternate;
+    uint32_t env_hold;
+    uint32_t env_pause;
+    uint32_t env_reset;
 
-	uint32 noise_seed;
-	uint32 noise_count;
-	uint32 noise_freq;
+    uint32_t env_freq;
+    uint32_t env_count;
 
-	/* rate converter */
-	uint32 realstep;
-	uint32 psgtime;
-	uint32 psgstep;
+    uint32_t noise_seed;
+    uint32_t noise_count;
+    uint32_t noise_freq;
 
-	/* I/O Ctrl */
-	uint32 adr;
-}
-PSG;
+    /* rate converter */
+    uint32_t realstep;
+    uint32_t psgtime;
+    uint32_t psgstep;
 
-EMU2149_API void PSG_set_quality(PSG* psg, uint32 q);
-EMU2149_API void PSG_set_rate(PSG* psg, uint32 r);
-EMU2149_API PSG* PSG_new(uint32 clk, uint32 rate);
-EMU2149_API void PSG_reset(PSG*);
-EMU2149_API void PSG_delete(PSG*);
-EMU2149_API void PSG_writeReg(PSG*, uint32 reg, uint32 val);
-EMU2149_API void PSG_writeIO(PSG* psg, uint32 adr, uint32 val);
-EMU2149_API uint8 PSG_readReg(PSG* psg, uint32 reg);
-EMU2149_API uint8 PSG_readIO(PSG* psg);
-EMU2149_API int16 PSG_calc(PSG*);
-EMU2149_API void PSG_setVolumeMode(PSG* psg, int type);
-EMU2149_API uint32 PSG_setMask(PSG*, uint32 mask);
-EMU2149_API uint32 PSG_toggleMask(PSG*, uint32 mask);
+    /* I/O Ctrl */
+    uint32_t adr;
 
-int32 PSG_getchanvol(int i);
+    /* output of channels */
+    int16_t ch_out[3];
 
+  } PSG;
+
+  void PSG_set_quality (PSG * psg, uint32_t q);
+  void PSG_set_rate (PSG * psg, uint32_t r);
+  PSG *PSG_new (uint32_t clk, uint32_t rate);
+  void PSG_reset (PSG *);
+  void PSG_delete (PSG *);
+  void PSG_writeReg (PSG *, uint32_t reg, uint32_t val);
+  void PSG_writeIO (PSG * psg, uint32_t adr, uint32_t val);
+  uint8_t PSG_readReg (PSG * psg, uint32_t reg);
+  uint8_t PSG_readIO (PSG * psg);
+  int16_t PSG_calc (PSG *);
+  void PSG_setVolumeMode (PSG * psg, int type);
+  uint32_t PSG_setMask (PSG *, uint32_t mask);
+  uint32_t PSG_toggleMask (PSG *, uint32_t mask);
+    
 #ifdef __cplusplus
 }
 #endif
